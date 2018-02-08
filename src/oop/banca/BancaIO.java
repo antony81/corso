cb6ec11866edae20730cc4c8145d32ff599fab58
Antonio <Consultant@LAPTOP-7NB8BGA5>
2018-02-08 10:44:29 +0100

    Aggiunta commenti.

--- src/oop/banca/BancaIO.java
+++ src/oop/banca/BancaIO.java
@@ -29,48 +29,72 @@ import java.util.logging.Logger;
 /**
  *
  * @author Antonio
+ * 
+ * Classe che contiene due metodi statici che consentono la serializzazione
+ * e la deserializzazione di un oggetto di tipo Banca.
  */
 public class BancaIO {
     
+    private final static Logger LOGGER = Logger.getLogger(BancaIO.class.getName()); // creazione di un logger
+    
+    /**
+     * 
+     * @param banca - l'oggetto da serializzare
+     * @return true se il salvataggio va a buin fine, false altrimenti
+     * 
+     * Metodo statico che permette di salvare un oggetto di tipo banca in
+     * memoria secondaria.
+     */
     public static boolean save(Banca banca) {
-        boolean res = false;
+        boolean esito = true;
+        
         //File file = new File("C:/dev/serializable/banca/bank.obj");
         File file = new File("./src/oop/saves/banca/bank.obj");
         
-        try ( FileOutputStream fos = new FileOutputStream(file);
+        try ( FileOutputStream fos = new FileOutputStream(file, true);
                 ObjectOutputStream oos = new ObjectOutputStream(fos); ) {
             oos.writeInt(banca.size());
             for(int i = 0; i < banca.size(); i++)
                 oos.writeObject(banca.read(i));
-            res = true;
+            
+            LOGGER.log(Level.INFO, "Salvataggio effettuato!");
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(BancaIO.class.getName()).log(Level.SEVERE, null, ex);
+            esito = false;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         } catch (IOException ex) {
-            Logger.getLogger(BancaIO.class.getName()).log(Level.SEVERE, null, ex);
+            esito = false;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         }
         
-        return res;
+        return esito;
     }
     
+    /**
+     * 
+     * @return l'oggetto di tipo Banca caricato dalla memoria secondaria
+     * 
+     * Metodo statico che legge un file in memoria secondaria in cui sono
+     * presenti i dati di un oggetto di tipo Banca.
+     */
     public static Banca load() {
         //File file = new File("C:/dev/serializable/banca/bank.obj");
         File file = new File("./src/oop/saves/banca/bank.obj");
-        Banca banca;
         
-        try (
-                FileInputStream fis = new FileInputStream(file);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-            ) {
+        Banca banca;
+        try ( FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis); ) {
             int size = ois.readInt();
             banca = new Banca(size);
             for(int i = 0; i < size; i++) {
-                ContoBancario bankAccount = (ContoBancario) ois.readObject();
-                banca.create(bankAccount);
+                ContoBancario cb = (ContoBancario) ois.readObject();
+                banca.create(cb);
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException ex) {
             banca = null;
-        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "errore", ex);
+        } catch (ClassNotFoundException | IOException ex) {
             banca = null;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         }
         
         return banca;
