3680ee6b3c4ddff5fb465ec9fc02e64653eac9d1
Antonio <>
2018-02-08 10:57:47 +0100

    Aggiunta commenti.

--- src/oop/banca/BancaIO.java
+++ src/oop/banca/BancaIO.java
@@ -32,45 +32,68 @@ import java.util.logging.Logger;
  */
 public class BancaIO {
     
+    private final static Logger LOGGER = Logger.getLogger(BancaIO.class.getName()); // creazione di un logger
+    
+    /**
+     * 
+     * @param banca - l'oggetto da salvare
+     * @return true se l'oggetto è stato salvato, false altrimenti
+     * 
+     * Metodo statico che consente di salvare un oggetto di tipo Banca in
+     * memoria secondaria. Se tutto va bene il metodo ritorna true.
+     */
     public static boolean save(Banca banca) {
-        boolean res = false;
+        boolean esito = true;
+        
         //File file = new File("C:/dev/serializable/banca/bank.obj");
         File file = new File("./src/oop/saves/banca/bank.obj");
         
-        try ( FileOutputStream fos = new FileOutputStream(file);
+        try ( FileOutputStream fos = new FileOutputStream(file, true);
                 ObjectOutputStream oos = new ObjectOutputStream(fos); ) {
             oos.writeInt(banca.size());
             for(int i = 0; i < banca.size(); i++)
                 oos.writeObject(banca.read(i));
-            res = true;
+            
+            LOGGER.log(Level.SEVERE, "Salvataggio andato a buon fine");
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(BancaIO.class.getName()).log(Level.SEVERE, null, ex);
+            esito = false;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         } catch (IOException ex) {
-            Logger.getLogger(BancaIO.class.getName()).log(Level.SEVERE, null, ex);
+            esito = false;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         }
         
-        return res;
+        return esito;
     }
     
+    /**
+     * 
+     * @return l'oggetto caricato dalla memoria secondaria
+     * 
+     * Metodo statico che consente di ricare un oggetto di tipo Banca
+     * precedentemente salvato. Il metodo ritorna 'null' se il file non c'è.
+     */
     public static Banca load() {
         //File file = new File("C:/dev/serializable/banca/bank.obj");
         File file = new File("./src/oop/saves/banca/bank.obj");
-        Banca banca;
         
-        try (
-                FileInputStream fis = new FileInputStream(file);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-            ) {
+        Banca banca;
+        try ( FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis); ) {
             int size = ois.readInt();
             banca = new Banca(size);
             for(int i = 0; i < size; i++) {
                 ContoBancario bankAccount = (ContoBancario) ois.readObject();
                 banca.create(bankAccount);
             }
-        } catch (FileNotFoundException e) {
+            
+            LOGGER.log(Level.SEVERE, "Caricamento andato a buon fine");
+        } catch (FileNotFoundException ex) {
             banca = null;
-        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "errore", ex);
+        } catch (IOException | ClassNotFoundException ex) {
             banca = null;
+            LOGGER.log(Level.SEVERE, "errore", ex);
         }
         
         return banca;
