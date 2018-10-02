996f5f8328abcc7ab02baacfa9182ed195f5b043
Antonio <Consultant@LAPTOP-7NB8BGA5>
2018-10-02 14:52:54 +0200

    Fix commenti

--- src/oop/trasporti/AbstractVeicolo.java
+++ src/oop/trasporti/AbstractVeicolo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Consultant
+ * Copyright (C) 2018 Antonio
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ import java.util.Objects;
 
 /**
  *
- * @author Consultant
+ * @author Antonio
  */
 public abstract class AbstractVeicolo implements Veicolo {
     
