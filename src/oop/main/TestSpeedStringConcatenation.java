5842c9041f74438557d13667154eafcd26276f55
Antonio <Consultant@LAPTOP-7NB8BGA5>
2018-10-10 16:07:08 +0200

    primo commit

--- src/oop/main/TestSpeedStringConcatenation.java
+++ src/oop/main/TestSpeedStringConcatenation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Consultant
+ * Copyright (C) 2018 Antonio
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ import oop.thread.string.SpeedStringConcatenation;
 
 /**
  *
- * @author Consultant
+ * @author Antonio
  */
 public class TestSpeedStringConcatenation {
 
