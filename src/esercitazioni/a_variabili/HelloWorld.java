36800f7d8119ed1d20c6e3ade4c8e3c17b1dde46
Antonio <Consultant@LAPTOP-7NB8BGA5>
2019-01-24 17:54:56 +0100

    added comments

--- src/esercitazioni/a_variabili/HelloWorld.java
+++ src/esercitazioni/a_variabili/HelloWorld.java
@@ -1,43 +1,50 @@
 /*
-* Copyright (C) 2017 Antonio
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2017 Antonio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package esercitazioni.a_variabili;
 
 /**
+ * Classe che stampa a video una stringa.
+ * Classico esempio per iniziare a conoscere il linguaggio Java.
+ * 
+ * Questo è un commento javadoc.
+ * I commenti scritti in questo modo possono essere tradotti in
+ * documentazione, generando dei file HTML da cui leggerla.
  *
  * @author Antonio
  */
 public class HelloWorld {
 
     /**
+     * Metodo main che contiene l'algoritmo per stampare a video una stringa.
      * 
-     * Questo è un commento javadoc
-     * Il programma stampa a video la Stringa Hello World
-     * 
-     * @param args the command line arguments
+     * @param args array di stringhe con gli argomenti che si possono passare dal prompt dei comandi
      * 
      */
     public static void main(String[] args) {
-        // Questo è un commento mono riga
-        System.out.println("Hello World");
+        /* 
+         * Questo è un commento multi riga.
+         * I commenti scritti in questo modo sono utili per aggiungere
+         * maggiori informazioni inerenti all'algoritmo che si sta scrivendo,
+         * in modo che si capisca meglio cosa contiene.
+         * In questo caso c'è l'istruzione per stampare a video sul terminale.
+         */
+        System.out.println("Hello World!\rCiao");
         
-        /*
-        Questo è un commento
-        multi riga
-        */
+        // Questo è un commento mono riga. Ritornando a capo si può riscrivere codice Java.
     }
     
 }
--- src/esercitazioni/a_variabili/InfoSistema.java
+++ src/esercitazioni/a_variabili/InfoSistema.java
@@ -1,27 +1,27 @@
 /*
-* Copyright (C) 2017 Antonio
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2017 Antonio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package esercitazioni.a_variabili;
 
 /**
+ * Classe che, con l'ausilio della classe System (classe della libreria standard
+ * di Java), ricava e stampa le proprietà del sistema operativo su cui viene
+ * eseguito.
  *
  * @author Antonio
- * 
- * Programma che ricava e stampa le proprietà del sistema operativo su cui
- * viene eseguito. Si fa uso della classe System.
  */
 public class InfoSistema {
     
